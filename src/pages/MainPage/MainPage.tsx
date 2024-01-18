8837fedc4a81c426d9987ea2343160998fbb3170
Andrey Pozdnyshev <dyusss@gmail.com>
2024-01-18 18:12:17 +0300

    static db.json

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,13 +1,32 @@
-import { useMemo } from 'preact/hooks';
+import { useEffect, useMemo, useState } from 'preact/hooks';
 import { Filters } from '../../components/Filters/Filters';
 import { List } from '../../components/List/List';
 import { AppStateContext, getDefaultAppState } from '../../contexts/AppState';
 
 import './MainPage.css';
+import { Game } from '../../types/Game';
 
-import db from './db.json';
+type DB = {
+  games: Game[];
+  date: string;
+};
 
 export const MainPage = () => {
+  const [db, setDb] = useState<DB | null>(null);
+  useEffect(() => {
+    const fetchDB = async () => {
+      const response = await fetch('/db.json');
+      setDb(await response.json());
+    };
+    fetchDB();
+  }, []);
+
+  console.log(db);
+
+  if (!db) {
+    return null;
+  }
+
   const state = useMemo(() => ({
     ...getDefaultAppState(),
     availableStatuses: [...new Set(db.games.map((item) => item.status))],
